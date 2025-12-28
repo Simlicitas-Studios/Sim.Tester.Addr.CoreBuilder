a35a8ba6136ad812a2bc86342eda4d36ea72e451
pasdaa13 <pasdaa13@gmail.com>
2025-12-28 16:52:49 +0100

    initial commit

---
+++ Assets/Core/Runtime/CoreEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class CoreEnemy : MonoBehaviour
+    {
+        public CoreEnemyStats Stats;
+
+        private void Awake()
+        {
+            Debug.Log($"CoreEnemy initialized with {Stats?.Health ?? 0} HP");
+        }
+    }
+}
---
+++ Assets/Core/Runtime/CoreEnemyStats.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    [CreateAssetMenu(fileName = "SomeEnemyStats", menuName = "Sim/Core/Enemy Stats", order = 0)]
+    public class CoreEnemyStats : ScriptableObject
+    {
+        public int Health;
+    }
+}
